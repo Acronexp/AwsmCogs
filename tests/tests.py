fc49d8732d41ccc738bfad9dc5497e89946d2bb8
GitAcrown <Acrown@outlook.fr>
2021-02-05 05:04:11 +0100

    Tests divers

---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+import discord
+from typing import List, Tuple, NewType
+import logging
+from redbot.core import Config, commands, checks, errors
+
+logger = logging.getLogger("red.AwsmCogs.tests")
+
+Members = NewType('Members', Tuple[discord.Member])
+
+class Dungeon(commands.Cog):
+    """Jeu de donjons procéduraux en équipe (ou solo)"""
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @commands.command()
+    async def multiplemembers(self, ctx, *members: Members):
+        """Teste la détection de plusieurs membres dans une commande"""
+        if members:
+            txt = ""
+            for p in members:
+                txt += f"{p.name}\n"
+            return await ctx.send(txt)
+        await ctx.send("**Rien à afficher**")
