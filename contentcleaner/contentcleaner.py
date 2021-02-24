500626878cee77d9bfabc85daa8ccae4ede7b370
GitAcrown <Acrown@outlook.fr>
2021-02-24 23:34:06 +0100

    Ajout du raccourcisseur auto. d'URL

--- contentcleaner/contentcleaner.py
+++ contentcleaner/contentcleaner.py
@@ -1,11 +1,13 @@
 import asyncio
 import logging
 import re
+from urllib import parse
 
 import discord
 import os
 import urllib.request
 
+import requests
 from redbot.core.data_manager import cog_data_path
 
 from redbot.core import Config, commands, checks, errors
@@ -22,7 +24,8 @@ class ContentCleaner(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=736144321857978388, force_registration=True)
 
-        default_guild = {"vocaroo": False}
+        default_guild = {"vocaroo": False,
+                         "shorten": False}
         self.config.register_guild(**default_guild)
 
         self.temp = cog_data_path(self) / "temp"
@@ -38,6 +41,16 @@ class ContentCleaner(commands.Cog):
             raise
         return path
 
+    def shorten_link(self, link: str):
+        key = 'a202f4f51e7d72c5826e2fcf649e6c3cc58e1'
+        url = parse.quote(link)
+        r = requests.get('http://cutt.ly/api/api.php?key={}&short={}'.format(key, url))
+        result = r.json()['url']
+        if result['status'] == 7:
+            return result['shortLink']
+        else:
+            return None
+
     @commands.group(name="contentcleaner", aliases=["cclean"])
     async def _content_cleaner(self, ctx):
         """Commandes de gestion des triggers de correction auto. de liens et contenus"""
@@ -52,10 +65,21 @@ class ContentCleaner(commands.Cog):
             await ctx.send("**Activé** » Les MP3 Vocaroo seront téléchargés et uploadés directement sur Discord")
         await self.config.guild(ctx.guild).vocaroo.set(not param)
 
+    @_content_cleaner.command()
+    async def shorten(self, ctx):
+        """Activer/désactiver la réduction auto. des liens trop longs"""
+        param = await self.config.guild(ctx.guild).shorten()
+        if param:
+            await ctx.send("**Désactivé** » Les URL trop longs ne seront pas remplacés")
+        else:
+            await ctx.send("**Activé** » Les URL trop longs (+ 150 caractères) postés indépendamment seront raccourcis automatiquent")
+        await self.config.guild(ctx.guild).shorten.set(not param)
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.guild:
             guild = message.guild
+            channel = message.channel
             params = await self.config.guild(guild).all()
             if params["vocaroo"]:
                 urls = re.compile(r'(https?://(?:vocaroo\.com|voca\.ro)/\S*)', re.DOTALL | re.IGNORECASE).findall(message.content)
@@ -70,3 +94,17 @@ class ContentCleaner(commands.Cog):
 
                         for p in paths:
                             os.remove(p)
+            if params['shorten']:
+                content = message.content
+                if "http" in content:
+                    scan = re.compile(r'(https?://\S*\.\S*)', re.DOTALL | re.IGNORECASE).findall(content)
+                    if scan:
+                        url = scan[0]
+                        if len(content) <= len(url) >= 150:
+                            try:
+                                await message.delete()
+                            except:
+                                pass
+                            else:
+                                await channel.send(self.shorten_link(url))
+
